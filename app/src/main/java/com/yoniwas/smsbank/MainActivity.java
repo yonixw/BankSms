764c4d7c08bc31f45c3c1879682fb2d06c448fc7
yonixw <yonixw@gmail.com>
2018-06-24 20:24:32 +0300

    added manual reading of sms

--- app/src/main/java/com/yoniwas/smsbank/MainActivity.java
+++ app/src/main/java/com/yoniwas/smsbank/MainActivity.java
@@ -2,12 +2,24 @@ package com.yoniwas.smsbank;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText txtboxFilters;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtboxFilters = (EditText)findViewById(R.id.txtFilters);
+    }
+
+
+
+    public void btnReadClick(View v) {
+        SmsReaderHelper.getAllSms(v.getContext(),txtboxFilters.getText().toString().split(";"));
     }
 }
---
+++ app/src/main/java/com/yoniwas/smsbank/SmsReaderHelper.java
@@ -0,0 +1,64 @@
+package com.yoniwas.smsbank;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.provider.Telephony;
+import android.widget.Toast;
+
+import java.util.Date;
+import java.util.List;
+
+public class SmsReaderHelper {
+    public  static boolean arrHas(String[] arr, String val) {
+        for (String item : arr){
+            if (item.equals(val.trim())) return  true;
+        }
+        return  false;
+    }
+
+    public static void getAllSms(Context context, String[] names) {
+
+        ContentResolver cr = context.getContentResolver();
+        Cursor c = cr.query(Telephony.Sms.CONTENT_URI, null, null, null, null);
+
+        int totalSMS = 0;
+        int filteredSMS = 0;
+
+        if (c != null) {
+            totalSMS = c.getCount();
+            if (c.moveToFirst()) {
+                for (int j = 0; j < totalSMS; j++) {
+                    String smsDate = c.getString(c.getColumnIndexOrThrow(Telephony.Sms.DATE));
+                    String number = c.getString(c.getColumnIndexOrThrow(Telephony.Sms.ADDRESS));
+                    String body = c.getString(c.getColumnIndexOrThrow(Telephony.Sms.BODY));
+                    Date dateFormat= new Date(Long.valueOf(smsDate));
+
+                    String type;
+                    switch (Integer.parseInt(c.getString(c.getColumnIndexOrThrow(Telephony.Sms.TYPE)))) {
+                        case Telephony.Sms.MESSAGE_TYPE_INBOX:
+                            type = "inbox";
+                            break;
+                        case Telephony.Sms.MESSAGE_TYPE_SENT:
+                            type = "sent";
+                            break;
+                        case Telephony.Sms.MESSAGE_TYPE_OUTBOX:
+                            type = "outbox";
+                            break;
+                        default:
+                            break;
+                    }
+
+                    if (arrHas(names,number)) filteredSMS++;
+                    c.moveToNext();
+                }
+            }
+
+            c.close();
+            Toast.makeText(context, "Found " + filteredSMS + " filtered sms by sender", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(context, "No message was found at all!", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
+
